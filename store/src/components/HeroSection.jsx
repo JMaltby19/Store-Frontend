10b93336148d655f6fb464e362bf40cf7376fe7b
JayM1926 <71045580+JayM1926@users.noreply.github.com>
2024-03-22 19:54:00 +0000

    updated hero section text responsiveness

--- store/src/components/HeroSection.jsx
+++ store/src/components/HeroSection.jsx
@@ -30,16 +30,16 @@ export const HeroSection = () => {
 			/>
 			<div className="absolute top-20 flex justify-center w-full z-1 p-1 md:bottom-10 md:left-15">
 				<div>
-					<h1 className=" font-semibold">iPhone 15 Pro</h1>
-					<h2 className=" font-normal text-3xl">
+					<h1 className="text-xl font-semibold md:text-5xl">iPhone 15 Pro</h1>
+					<h2 className="text-xl font-normal md:text-3xl">
 						Titanium. So strong. So light. So Pro
 					</h2>
 				</div>
 			</div>
-			<div className="absolute bottom-10 left-10 flex justify-start w-full z-10 p-1 md:bottom-10 md:left-15">
+			<div className="absolute bottom-3 left-6 flex justify-start w-full z-10 p-1 md:bottom-10 md:left-15">
 				<div className=" max-w-sm">
 					<Link to={"/products/Smartphones"}>
-						<button className="bg-white text-black py-2 px-4 rounded text-sm md:text-base">
+						<button className="bg-white text-black py-1 px-2 rounded text-sm md:py-2 md:px-4 md:text-base">
 							Shop now
 						</button>
 					</Link>
