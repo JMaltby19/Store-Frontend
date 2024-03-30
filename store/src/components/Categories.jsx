6602a699d469f21354cd13f1fcfad14d6037d1fa
JayM1926 <71045580+JayM1926@users.noreply.github.com>
2024-03-30 18:57:32 +0000

    updated - installed gsap animations

--- store/src/components/Categories.jsx
+++ store/src/components/Categories.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addCategory } from "../features/categorySlice"; // Adjust this to the action you want to use
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export const Categories = ({ category }) => {
 	const dispatch = useDispatch();
@@ -10,12 +13,38 @@ export const Categories = ({ category }) => {
 		dispatch(addCategory(category.category)); // Assuming you want to add this category
 	};
 
+	gsap.registerPlugin(useGSAP, ScrollTrigger);
+	const categoryRef = useRef(null);
+
+	useGSAP(() => {
+		gsap.from(categoryRef.current, {
+			duration: 1,
+			y: 100,
+			opacity: 0,
+			scrollTrigger: {
+				trigger: categoryRef.current,
+				start: "top 85%",
+				end: "bottom 60%",
+				toggleActions: "play none none reverse",
+			},
+			stagger: 0.15,
+		});
+		// gsap.from(categoryRef.current, {
+		// 	duration: 3,
+		// 	y: -150,
+		// 	opacity: 0,
+		// 	ease: "power4.out",
+		// 	stagger: 0.2,
+		// });
+	});
+
 	return (
 		<div
 			className="bg-white rounded-lg w-72 md:w-72 lg:max-w-6xl xl:max-w-7xl"
 			onClick={handleCategoryClick}
+			ref={categoryRef}
 		>
-			<div className="mx-auto px-4 py-10 sm:px-6 sm:py-12 lg:max-w-screen-7xl">
+			<div className=" mx-auto px-4 py-10 sm:px-6 sm:py-12 lg:max-w-screen-7xl">
 				<div className="">
 					{/* {products.map((product) => ( */}
 					<Link to={`/products/${category.category}`}>
--- store/src/components/HeroSection.jsx
+++ store/src/components/HeroSection.jsx
@@ -1,45 +1,71 @@
-// import React from "react";
-
-// export const HeroSection = () => {
-// 	return (
-// 		<div className="relative max-w-screen-2xl object-contain ">
-// 			<img
-// 				src="https://res.cloudinary.com/ddijiwxw5/image/upload/v1704925030/Apple-iPhone-15-Pro-Hero-Gear_ulsohl.jpg"
-// 				alt=""
-// 				className="w-full"
-// 			/>
-// 			<div className="absolute bottom-5 left-5 z-10">
-// 				<button className="bg-white text-black py-2 px-4 rounded">
-// 					Shop now
-// 				</button>
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export const HeroSection = () => {
+	const titleRef = useRef(null);
+	const subTitleRef = useRef(null);
+	const buttonRef = useRef(null);
+
+	gsap.registerPlugin(useGSAP);
+
+	useGSAP(() => {
+		gsap.from(".title", {
+			duration: 3,
+			y: -150,
+			opacity: 0,
+			ease: "power4.out",
+			stagger: 0.2,
+		});
+		gsap.from(subTitleRef.current, {
+			duration: 3,
+			y: -100,
+			opacity: 0,
+			ease: "power4.out",
+			stagger: 0.2,
+		});
+		gsap.from(buttonRef.current, {
+			duration: 3,
+			x: -200,
+			opacity: 0,
+			ease: "power4.out",
+			stagger: 0.2,
+		});
+		gsap.from(".hero-image", {
+			duration: 3,
+			scale: 1.3,
+			ease: "power2.out",
+		});
+	});
+
 	return (
-		<div className="relative w-full max-w-screen-2xl object-contain ">
+		<div className="relative w-full max-w-screen-2xl object-contain">
 			<img
 				src="https://res.cloudinary.com/ddijiwxw5/image/upload/v1705258121/f-4d62f49966d4af93a651379f09657fba6cc3c770_usiraj.jpg"
 				alt="iPhone 15 Pro"
-				className=" h-auto object-cover brightness-[.60]"
+				className="hero-image h-auto object-cover brightness-[.60]"
 			/>
 			<div className="absolute top-20 flex justify-center w-full z-1 p-1 md:bottom-10 md:left-15">
 				<div>
-					<h1 className="text-xl font-semibold md:text-5xl">iPhone 15 Pro</h1>
-					<h2 className="text-xl font-normal md:text-3xl">
+					<h1
+						ref={titleRef}
+						className="title text-xl font-semibold md:text-5xl"
+					>
+						iPhone 15 Pro
+					</h1>
+					<h2 ref={subTitleRef} className="text-xl font-normal md:text-3xl">
 						Titanium. So strong. So light. So Pro
 					</h2>
 				</div>
 			</div>
 			<div className="absolute bottom-3 left-6 flex justify-start w-full z-10 p-1 md:bottom-10 md:left-15">
-				<div className=" max-w-sm">
+				<div className="max-w-sm">
 					<Link to={"/products/Smartphones"}>
-						<button className="bg-white text-black py-1 px-2 rounded text-sm md:py-2 md:px-4 md:text-base">
+						<button
+							ref={buttonRef}
+							className="bg-white text-black py-1 px-2 rounded text-sm md:py-2 md:px-4 md:text-base"
+						>
 							Shop now
 						</button>
 					</Link>
@@ -48,3 +74,35 @@ export const HeroSection = () => {
 		</div>
 	);
 };
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// export const HeroSection = () => {
+// 	return (
+// 		<div className="relative w-full max-w-screen-2xl object-contain ">
+// 			<img
+// 				src="https://res.cloudinary.com/ddijiwxw5/image/upload/v1705258121/f-4d62f49966d4af93a651379f09657fba6cc3c770_usiraj.jpg"
+// 				alt="iPhone 15 Pro"
+// 				className=" h-auto object-cover brightness-[.60]"
+// 			/>
+// 			<div className="absolute top-20 flex justify-center w-full z-1 p-1 md:bottom-10 md:left-15">
+// 				<div>
+// 					<h1 className="text-xl font-semibold md:text-5xl">iPhone 15 Pro</h1>
+// 					<h2 className="text-xl font-normal md:text-3xl">
+// 						Titanium. So strong. So light. So Pro
+// 					</h2>
+// 				</div>
+// 			</div>
+// 			<div className="absolute bottom-3 left-6 flex justify-start w-full z-10 p-1 md:bottom-10 md:left-15">
+// 				<div className=" max-w-sm">
+// 					<Link to={"/products/Smartphones"}>
+// 						<button className="bg-white text-black py-1 px-2 rounded text-sm md:py-2 md:px-4 md:text-base">
+// 							Shop now
+// 						</button>
+// 					</Link>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// };
